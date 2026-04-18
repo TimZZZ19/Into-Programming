3c47ee1f515d25e2655f0b9dddde85f8b6e96e78
TimsUbuntu <TimsUbuntu@gmail.com>
2026-04-19 00:27:23 +0800

    added reverseStr

---
+++ pratice_4.18.26/java.java
@@ -0,0 +1,11 @@
+
+
+
+public class java {
+    
+    public static void main(String[] args) {
+        int curr = 2;
+        System.out.print("" + curr);
+
+    }
+}
---
+++ pratice_4.18.26/replaceStr.java
@@ -0,0 +1,13 @@
+public class replaceStr {
+    
+    public static void main(String[] args) {
+        // Your code here
+        // Consider the method replaceStr, which is instended to 
+        // return a string that is identical to the original string
+        // except when there are occurrences of some character, in
+        // which case all those occurrences will be repaced by another
+        // give character. 
+
+    }
+}
+
